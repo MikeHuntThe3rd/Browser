34b2a63ff4e1f82d121f118092a628ffc27d070a
Simák Balázs <simakbalazsi@gmail.com>
2026-01-04 12:11:07 +0100

    holy shit i might have fixed it

---
+++ Tab.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Browser
+{
+    public class Tab : INotifyPropertyChanged
+    {
+        private string _url = "";
+        public string Url
+        {
+            get => _url;
+            set { _url = value; OnPropertyChanged(); }
+        }
+
+        public string Title { get; set; } = "New Tab";
+
+        public ObservableCollection<string> History { get; } = new ObservableCollection<string>();
+        public int HistoryIndex { get; set; } = 0;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] string name = null)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+
+}
---
+++ TabDragManager.cs
@@ -0,0 +1,21 @@
+﻿using Dragablz;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Browser
+{
+    public static class TabDragManager
+    {
+        public static InterTabController CreateController()
+        {
+            return new InterTabController
+            {
+                Partition = "BrowserTabs"
+            };
+        }
+    }
+}
